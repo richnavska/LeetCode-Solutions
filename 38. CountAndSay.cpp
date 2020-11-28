5da8c7cac5ce4964368ba9e950dc04b871c7b2d3
richnavska <72523311+richnavska@users.noreply.github.com>
2020-11-28 14:38:57 +0100

    Update 38. CountAndSay.cpp

--- 38. CountAndSay.cpp
+++ 38. CountAndSay.cpp
@@ -1,6 +1,6 @@
 /*
 The count-and-say sequence is the sequence of integers beginning as follows:
-1, 11, 21, 1211, 111221, ...
+1, 11, 21, 1211, 111221, ....
 1 is read off as one 1 or 11.
 11 is read off as two 1s or 21.
 21 is read off as one 2, thone 1 or 12vjhvj11.
